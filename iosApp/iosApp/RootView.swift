0bb5a608a37482502b3e7450c99bc5d33e30cdb4
tabatsky <e.tabatsky@gmail.com>
2025-08-04 07:03:05 +1000

    some code duplication removed

--- iosApp/iosApp/RootView.swift
+++ iosApp/iosApp/RootView.swift
@@ -52,7 +52,7 @@ struct RootView: View {
                 }
                 .background(self.appState.theme.colorBg)
             } else {
-                if self.appState.localState.isDrawerOpen {
+                let stackView = {
                     StackView(
                         stackValue: StateValue(root.stack),
                         getTitle: { _ in
@@ -102,59 +102,14 @@ struct RootView: View {
                             }
                         }
                     )
+                }
+                if self.appState.localState.isDrawerOpen {
+                    stackView()
                     .highPriorityGesture(TapGesture().onEnded {
                         self.performAction(DrawerClick())
                     })
                 } else {
-                    StackView(
-                        stackValue: StateValue(root.stack),
-                        getTitle: { _ in
-                            return ""
-                        },
-                        onBack: root.onBackClicked,
-                        childContent: {
-                            switch $0 {
-                            case let child as RootComponentChild.StartChild: StartScreenView(
-                                startComponent: child.component,
-                                theme: self.appState.theme,
-                                onPerformAction: self.performAction
-                            )
-                            case let child as RootComponentChild.SongListChild: SongListView(
-                                songListComponent: child.component,
-                                theme: self.appState.theme,
-                                localState: self.appState.localState,
-                                onPerformAction: self.performAction
-                            ).navigationBarBackButtonHidden(true)
-                            case let child as RootComponentChild.SongTextChild: SongTextView(
-                                songTextComponent: child.component,
-                                theme: self.appState.theme,
-                                song: self.appState.localState.currentSong!,
-                                position: Int(self.appState.localState.currentSongIndex),
-                                songCount: Int(self.appState.localState.currentCount),
-                                onPerformAction: self.performAction
-                            ).navigationBarBackButtonHidden(true)
-                            case let child as RootComponentChild.CloudSearchChild: CloudSearchView(
-                                cloudSearchComponent: child.component,
-                                theme: self.appState.theme,
-                                cloudState: self.appState.cloudState,
-                                onPerformAction: self.performAction
-                            ).navigationBarBackButtonHidden(true)
-                            case let child as RootComponentChild.CloudSongTextChild: CloudSongTextView(
-                                cloudSongTextComponent: child.component,
-                                theme: self.appState.theme,
-                                cloudState: self.appState.cloudState,
-                                onPerformAction: self.performAction
-                            ).navigationBarBackButtonHidden(true)
-                            case let child as RootComponentChild.SettingsChild: SettingsView(
-                                settingsComponent: child.component,
-                                theme: self.appState.theme,
-                                onPerformAction: self.performAction,
-                                forceReload: self.forceReload
-                            ).navigationBarBackButtonHidden(true)
-                            default: EmptyView()
-                            }
-                        }
-                    )
+                    stackView()
                 }
             }
             NavigationDrawer(
